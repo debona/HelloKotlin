3bdd89e31b6204b542df43a3e6b3ce9b5d150c4f
Thomas DE BONA <thomas.debona@gmail.com>
2015-02-19 09:29:35 +0100

    Remove useless code and comments

--- app/src/main/kotlin/fr/debona/hellothere/DrawerFragment.kt
+++ app/src/main/kotlin/fr/debona/hellothere/DrawerFragment.kt
@@ -8,15 +8,11 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 
-/**
- * Created by debona on 17/02/15.
- */
 public class DrawerFragment:Fragment() {
 
     override fun onCreateView(inflater:LayoutInflater, container:ViewGroup?, savedInstanceState:Bundle?):View {
         val rootView = inflater.inflate(R.layout.fragment_drawer, container, false)
-//        val text_view_hello = rootView.findViewById(R.id.text_view_hello) as TextView
-//        text_view_hello.setText("text_view_hello")
+
         rootView.setOnClickListener(object: View.OnClickListener {
             override fun onClick(v: View) {
                 Toast.makeText(getActivity(), "Drawer menu clicked!", Toast.LENGTH_SHORT).show()
